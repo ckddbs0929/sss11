08580aabff9063971c50e1245fafc143d1ac4690
saemmilee <ckddbs0929@naver.com>
2021-11-11 16:35:15 +0900

    Initial commit

---
+++ src/test/java/shop/sss/SssApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.sss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
