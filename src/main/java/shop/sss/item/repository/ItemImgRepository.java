c8b1f4b4383d010cabd49bb5ae0c89d0d0384757
saemmilee <ckddbs0929@naver.com>
2021-11-22 15:38:34 +0900

    spring jpa로 개발

---
+++ src/main/java/shop/sss/item/repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package shop.sss.item.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shop.sss.item.entity.ItemImg;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+}
